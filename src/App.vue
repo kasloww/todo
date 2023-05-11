4e24a324e3c3075f701b36547ebdac8a5333d546
Kasim Davidson <kdavidson@brooklynsteamcenter.org>
2023-05-11 14:54:38 -0400

    its done!

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,51 @@
 <script setup>
+import {ref} from 'vue'
+
+let todos = ref([])
+let newtodos = ref('')
+
+function clicke() {
+  todos.value.push(newtodos.value)
+
+  newtodos.value = ''
+  
+  
+}
+
+function getout (index) {
+  todos.value.splice(index, 1)
+}
+
 
 </script>
 
 <template>
-  <h1>My ToDo</h1>
+  <h1>What Do You Need Done Today?</h1>
+  
+  <ul>
+  <li v-for="(todo, index) in todos">
+    <button @click="getout(index)">❌</button>
+    {{ index }}
+    {{todo}}
+  </li>
+  </ul>
+
+  <input v-model="newtodos" @keydown.enter="clicke">
+  <button @click="clicke">Add Todo</button>
 </template>
 
-<style scoped>
-
+<style>
+h1 {
+  text-decoration: underline;
+  font-family: 'Times New Roman', Times, serif;
+}
+button {
+    background-color:darkblue;
+    color:azure;
+    cursor: pointer;
+    font-family: 'Instrument Serif', serif;
+}
+li {
+  font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+}
 </style>
